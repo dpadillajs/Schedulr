75c7189b3f8effb5b1874a0498e90ff855ec6f3f
DPadillaNYG <DPadilla.NYG@gmail.com>
2019-02-12 18:55:45 -0500

    HTML/CSS

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,10 +4,7 @@ module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+      res.render("index");
     });
   });
 
