1720f06c2557b35abd797752e5780cb076a7b77d
LBDCode <libby.duggan@gmail.com>
2019-02-12 18:59:56 -0500

    adding models, api routes, etc

--- models/appointment.js
+++ models/appointment.js
@@ -1,10 +1,9 @@
 module.exports = function(sequelize, DataTypes) {
-    var Appointment = sequelize.define("Appointment", {
-        customer_id: DataTypes.INTEGER,
-        business_id: DataTypes.INTEGER,
-        start_time: DataTypes.DATE,
-        note: DataTypes.TEXT
-    });
-    return Appointment;
-  };
-  
+  var Appointment = sequelize.define("Appointment", {
+    customer_id: DataTypes.INTEGER,
+    business_id: DataTypes.INTEGER,
+    start_time: DataTypes.DATE,
+    note: DataTypes.TEXT
+  });
+  return Appointment;
+};
--- models/schema.sql
+++ models/schema.sql
@@ -7,7 +7,7 @@ CREATE DATABASE testdb;
 USE appointmentdb;
 
 INSERT into appointments(customer_id, business_id, start_time, note, createdAt, updatedAt)
-VALUES(1, 1, current_date(), "This is the first test appt", current_time(), current_time());
+VALUES(1, 1, current_time(), "This is the first test appt", current_time(), current_time());
 
 INSERT into appointments(customer_id, business_id, start_time, note, createdAt, updatedAt)
 VALUES(2, 2, current_time(), "This is the second test appt", current_time(), current_time());
