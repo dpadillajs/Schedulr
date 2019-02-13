5127ffc06c8e0f70a53360ad587141e51decfa46
DPadillaNYG <DPadilla.NYG@gmail.com>
2019-02-12 19:14:55 -0500

    Fixed Travis

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,6 +5,7 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index");
+      console.log(dbExamples);
     });
   });
 
