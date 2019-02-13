3a9a2bb7bed7894d64b87e08981b462d94a12890
LBDCode <libby.duggan@gmail.com>
2019-02-12 19:04:26 -0500

    adding models, api routes, etc

--- models/schema.sql
+++ models/schema.sql
@@ -6,11 +6,4 @@ CREATE DATABASE testdb;
 
 USE appointmentdb;
 
-INSERT into appointments(customer_id, business_id, start_time, note, createdAt, updatedAt)
-VALUES(1, 1, current_time(), "This is the first test appt", current_time(), current_time());
-
-INSERT into appointments(customer_id, business_id, start_time, note, createdAt, updatedAt)
-VALUES(2, 2, current_time(), "This is the second test appt", current_time(), current_time());
-
-
 SELECT * FROM appointments;
