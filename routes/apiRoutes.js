51b35af9907ddc4ffd6d1fc47046c3c3ce6fb1c4
LBDCode <libby.duggan@gmail.com>
2019-02-14 17:46:46 -0500

    appts crud redo

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,49 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all appointments
+  app.get("/api/appointments", function(req, res) {
+    db.Appointment.findAll({}).then(function(dbAppt) {
+      res.json(dbAppt);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Create a new appointment
+  app.post("/api/appointments", function(req, res) {
+    console.log(req.body);
+    db.Appointment.create({
+      customer_id: req.body.customer_id,
+      business_id: req.body.business_id,
+      start_time: req.body.start_time,
+      note: req.body.note
+    }).then(function(dbAppt) {
+      res.json(dbAppt);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
+  app.put("/api/appointments", function(req, res) {
+    console.log(req.body);
+    db.Appointment.update(
+      {
+        start_time: req.body.start_time,
+        note: req.body.note
+      },
+      {
+        where: {
+          id: req.body.id
+        }
+      }
+    ).then(function(dbAppt) {
+      res.json(dbAppt);
+    });
+  });
+
+  // Delete an appointment by id
+  app.delete("/api/appointments/:id", function(req, res) {
+    db.Appointment.destroy({ where: { id: req.params.id } }).then(function(
+      dbAppt
     ) {
-      res.json(dbExample);
+      res.json(dbAppt);
     });
   });
 };
