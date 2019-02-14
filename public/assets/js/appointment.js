14a63b9c740f50ed8d51e345490d922c36bfce2e
LBDCode <libby.duggan@gmail.com>
2019-02-14 12:13:49 -0500

    added update and delete func

---
+++ public/assets/js/appointment.js
@@ -0,0 +1,65 @@
+var $customer = $("#customer");
+var $startTime = $("#appt-date");
+var $note = $("#note");
+var $submitBtn = $("#submit");
+var $cancBtn = $("#cancel");
+
+var API = {
+  saveAppointment: function(appt) {
+    console.log("Saving 2");
+    return $.ajax({
+      headers: {
+        "Content-Type": "application/json"
+      },
+      type: "POST",
+      url: "/api/appointments",
+      data: JSON.stringify(appt)
+    });
+  },
+  getAppointments: function() {
+    return $.ajax({
+      url: "/api/appointments",
+      type: "GET"
+    });
+  },
+  deleteAppointment: function(id) {
+    return $.ajax({
+      url: "/api/appointments/" + id,
+      type: "DELETE"
+    });
+  }
+};
+
+var newAppointment = function(event) {
+  event.preventDefault();
+  console.log($startTime.val());
+  console.log("saving - newAppt 1");
+
+  var appt = {
+    customer_id: $customer.val().trim(),
+    business_id: $submitBtn.attr("data-id"),
+    start_time: $startTime.val().trim(),
+    note: $note.val().trim()
+  };
+
+  if (!(appt.customer_id && appt.business_id && appt.start_time && appt.note)) {
+    alert("Please fill out all fields");
+    return;
+  }
+
+  API.saveAppointment(appt).then(function() {
+    console.log("New Appt saved 3");
+    console.log(appt);
+  });
+};
+
+var deletedAppt = function() {
+  var apptID = $cancBtn.attr("data-id");
+  API.deleteAppointment(apptID).then(function() {
+    location.reload();
+  });
+};
+
+$submitBtn.on("click", newAppointment);
+
+$cancBtn.on("click", deletedAppt);
