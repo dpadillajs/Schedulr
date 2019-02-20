96c23163e48dc2400925dfaf104b412b120082b7
LBDCode <libby.duggan@gmail.com>
2019-02-20 14:10:59 -0500

    pop form w/ cust info

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -149,6 +149,35 @@ module.exports = function(app) {
       });
   });
 
+  // app.put("/api/new-customer", function(req, res) {
+  //   db.Customer.update(
+  //     {
+  //       email: req.body.email,
+  //       firstName: req.body.firstName,
+  //       lastName: req.body.lastName,
+  //       mName: req.body.mName,
+  //       phone: req.body.phone,
+  //       age: req.body.age,
+  //       gender: req.body.gender,
+  //       address: req.body.address,
+  //       zipcode: req.body.zipcode
+  //     },
+  //     {
+  //       where: {
+  //         id: req.body.id
+  //       }
+  //     }
+  //   )
+  //     .then(function(editCustomer) {
+  //       res.json(editCustomer);
+  //     })
+  //     .catch(function(err) {
+  //       if (err) {
+  //         res.json(err);
+  //       }
+  //     });
+  // });
+
   function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
       return next();
